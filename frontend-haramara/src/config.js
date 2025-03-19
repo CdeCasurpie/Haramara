beeb5cc0d409ec4c3bb5beae8d887532a16dcf10
Cesar Perales <c.casurpie@gmail.com>
2025-03-18 19:38:14 -0500

    config

--- frontend-haramara/src/config.js
+++ frontend-haramara/src/config.js
@@ -1,2 +1,2 @@
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "https://bf93-35-204-196-249.ngrok-free.app";
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "https://api.haramara.app";
 export default API_BASE_URL;
