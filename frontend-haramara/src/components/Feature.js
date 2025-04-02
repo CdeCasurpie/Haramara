c9a8061b851693c4a4ed5fbadfb2e37918b1726e
Cesar Perales <c.casurpie@gmail.com>
2025-04-02 10:38:48 -0500

    bug

--- frontend-haramara/src/components/Feature.js
+++ frontend-haramara/src/components/Feature.js
@@ -1,13 +1,7 @@
 import React from 'react';
-import styles from './feature.module.css';
 
 export default function Feature({ name, content }) {
   return (
-    <div className={styles.container}>
-        {name}
-        <div className={styles.content}>
-            {content}
-        </div>
-    </div>
+    <>feature</>
   );
 }
